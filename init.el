f6f6e95663a025cdd20a333c082bf1b6b9cf6a25
James Jackson <j.e.m.jackson@gmail.com>
2023-06-03 18:57:19 -0500

    Add a transiant map to jump between flymake errors.
    After hitting C-C e, you can hit [ or ] to jump between flymake errors.

--- init.el
+++ init.el
@@ -60,15 +60,42 @@
 	     :hook
 	     (rust-mode . eglot-ensure))
 
-;-------------------------------------------------------------------------------
-; Global keybindings
-;-------------------------------------------------------------------------------
+;;------------------------------------------------------------------------------
+;; Global keybindings
+;;------------------------------------------------------------------------------
 
 (global-set-key (kbd "C-c K") 'eldoc-doc-buffer)
 (global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c E") 'flymake-show-project-diagnostics)
-(global-set-key (kbd "C-c e [") 'flymake-goto-prev-error)
-(global-set-key (kbd "C-c e ]") 'flymake-goto-next-error)
+
+
+(defun transiant-error-map ()
+  (set-temporary-overlay-map
+   (let ((map (make-sparse-keymap)))
+     (define-key map (kbd "E") 'flymake-show-project-diagnostics)
+     (define-key map (kbd "[") 'my-prev-error)
+     (define-key map (kbd "]") 'my-next-error)
+     map)))
+
+
+
+(defun my-next-error ()
+  "Go to the next flymake error and set a temporary keybinding to jump between errors with []"
+  (interactive)
+   (flymake-goto-next-error)
+   (transiant-error-map))
+
+(global-set-key (kbd "C-c e ]")
+		'my-next-error)
+
+(defun my-prev-error ()
+  "Go to the previous flymake error and set a temporary keybinding to jump between errors with []"
+    (interactive)
+   (flymake-goto-prev-error)
+   (transiant-error-map))
+
+(global-set-key (kbd "C-c e [") 'my-prev-error)
+
 (global-set-key (kbd "C-c ^") 'beginning-of-line-text)
 
 (defun insert-pair-{} ()
