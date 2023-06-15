e7946f6cea78e0770d676485bb50ff6ef95019e8
James Jackson <j.e.m.jackson@gmail.com>
2023-06-15 17:50:06 -0500

    Added a library for writing and running advent-of-code problems.

---
+++ site-lisp/advent.el
@@ -0,0 +1,34 @@
+(define-skeleton advent-day-hs
+ "A template for advent of code days"
+ "Day: "
+ "import Game.Advent
+import Paths_" (skeleton-read "Project name: ") " (getDataFileName)
+
+datafile filename = getDataFileName filename >>= readFile
+
+{- | Part 1.  Short description of the problem.
+>>> d" str "p1 \"day" str "-ex.txt\"
+\"TODO\"
+-}
+d" str "p1 filename = do
+  input <- dataFile filename
+  return \"Day " str " Part 1 result\"
+
+{- | Part 2.  Short description of the problem.
+>>> d" str "p2 \"day" str "-ex.txt\"
+\"TODO\"
+-}
+d" str "p2 = do
+  input <- dataFile filename
+  return \"Day " str " Part 1 result\"
+
+
+day" str " :: Day
+day" str " =
+  Day
+    { dayName = \"" str "\",
+      dayPart1 = d" str "p1 "day" str ".txt",
+      dayPart2 = d" str "p1 "day" str ".txt",
+    }")
+
+
